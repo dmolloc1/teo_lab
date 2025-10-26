df65dcc299d9fce590f832fb213c510b24259cef
dmolloc1 <dmolloc@unsa.edu.pe>
2025-10-26 07:02:07 -0500

    add(main): Se agrego un countif para los numeros impares y se coloco la base de una funcion lambda

--- Lab07/Ejercicio1/main.cpp
+++ Lab07/Ejercicio1/main.cpp
@@ -1,13 +1,21 @@
 #include <QCoreApplication>
 #include <QVector>
 #include <algorithm>
+#include <ctime>
 using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QVector<int> num_list;//En este vector se almacenara los números creados
-    int impar=0;
+    int impar = 0;
+    srand(time(NULL));
+    for(int i = 0; i < 20; i++){
+        num_list.push_back(rand()%50);
+    }
 
+    impar = count_if(num_list.begin() , num_list.end(impar), [int num]{
+        return ()
+    });
 
     return a.exec();
 }
