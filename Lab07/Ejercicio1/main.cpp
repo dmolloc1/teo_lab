3fe8a03c721fb2b70b54ffcbbc426a8962f3f803
dmolloc1 <dmolloc@unsa.edu.pe>
2025-10-26 07:13:31 -0500

    error(main): Se corrigio un error de sintaxis pero sigue marcando error la funcion lambda

--- Lab07/Ejercicio1/main.cpp
+++ Lab07/Ejercicio1/main.cpp
@@ -7,14 +7,14 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QVector<int> num_list;//En este vector se almacenara los números creados
-    int impar = 0;
+
     srand(time(NULL));
     for(int i = 0; i < 20; i++){
         num_list.push_back(rand()%50);
     }
+    int impar = count_if(num_list.begin() , num_list.end(), [int n]{
 
-    impar = count_if(num_list.begin() , num_list.end(impar), [int num]{
-        return ()
+        return n < 20 && /(n % 2 != 0);
     });
 
     return a.exec();
