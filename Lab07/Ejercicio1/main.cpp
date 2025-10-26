6e7f94711e3c99a82d827cb70642b3731f9ce345
dmolloc1 <dmolloc@unsa.edu.pe>
2025-10-26 06:42:03 -0500

    feat(main): Vector de números y contador de primos creado

---
+++ Lab07/Ejercicio1/main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+#include <QVector>
+#include <algorithm>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QVector<int> num_list;//En este vector se almacenara los números creados
+    int impar=0;
+
+
+    return a.exec();
+}
