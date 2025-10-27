ffdce55cb9a706b3a074b1189892495eabdf6f98
dmolloc1 <dmolloc@unsa.edu.pe>
2025-10-26 23:04:30 -0500

    feat(windown_main.cpp): se declaro y lleno la lista paises y se agrego el nombre del pais a la lista en ui

--- Lab07/Ejercicio3/mainwindow.cpp
+++ Lab07/Ejercicio3/mainwindow.cpp
@@ -6,9 +6,24 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    países = {
+        {"México", "Español", "Ciudad de México"},
+        {"Colombia", "Español", "Bogotá"},
+        {"Perú", "Español", "Lima"},
+        {"Chile", "Español", "Santiago"},
+        {"Bolivia", "Español, Quechua, Aymara", "Sucre"}
+    };
+
+    for (const auto &n : paises) {
+        ui->list_paises->addItem(n.nombre);
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::showPais() {
+    ui->idioma_pais->showText();
 }
