42d811cf763bfae06c839848d2650c971002cd8f
dmolloc1 <dmolloc@unsa.edu.pe>
2025-11-02 17:13:45 -0500

    feat(List): metodo print implementado

---
+++ Lab08/List.h
@@ -0,0 +1,77 @@
+#include <iostream> 
+using namespace std;
+
+template <class T>
+class Node {
+private:
+    T data;//Informacion que contiene que puede ser de cualquier tipo
+    Node* next;//Puntero al siguiente nodo
+
+    //En una lista enlazada simple solo se puede recorrer en una direccion
+    Node(T d){
+        data = d;
+        next = nullptr;
+    }
+    
+    //getters y setters
+    T getData() {
+        return data;
+    }
+    Node* getNext() {
+        return next;
+    }
+    void setNext(Node* n) {
+        next = n;
+    }
+        
+};
+
+template <class T>
+class List {
+private:
+    Node<T>* head;//Primer nodo
+    Node<T>* tail;//Ultimo nodo
+public:
+    List() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    //Destructor de una lista
+    ~List() {
+        Node<T>* actual = head;//Puntero que recorre la lista
+        Node<T>* pivot = nullptr;//Puntero que guarda el siguiente nodo
+        
+        //Despues de recorrer cada nodo se guarda el siguiente en pivot y se elimina el actual
+        while (actual != nullptr) {
+            pivot = actual->getNext();
+            delete actual;
+            actual = pivot;
+        }
+    }
+
+    void insert(T nod){
+        //Si es el primer elemento se coloca su puntero en head y tail
+        if (head == nullptr) {
+            head = new Node<T>(nod);
+            tail = *head;
+            return;
+        } 
+        //Si ya hay elementos se coloca al final 
+        Node<T>* chain = new Node<T>(nod);
+        tail->setNext(chain);
+        tail = *chain;
+    }
+
+    void print(){
+        Node<T>* pivot = head;
+        //Recorre la lista desde el head usando un pivote
+        while (pivot != nullptr) {
+            cout << pivot->getData() << " ";
+
+            pivot = pivot->getNext();
+        }
+        cout << endl;
+    }
+
+};
---
+++ Lab08/main.cpp
@@ -0,0 +1,14 @@
+#include "List.h"
+using namespace std;
+
+int main() {
+    List<int> lista;
+
+    lista.insert(10);
+    lista.insert(20);
+    lista.insert(30);
+
+    cout << "Elementos lista enteros: " << ;
+    
+    return 0;
+}
